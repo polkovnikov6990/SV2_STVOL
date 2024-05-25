f2181d0a7bd32484fc99aa54884a598eddd24666
Alexey <polkovnikov6990@mail.ru>
2024-05-25 15:25:38 +0500

    Исправление ошибок в Application

--- gui/app_gui.py
+++ gui/app_gui.py
@@ -1,9 +1,10 @@
-# gui/app_gui.py
 import tkinter as tk
 from tkinter import ttk, filedialog, messagebox
 from camera.camera import Camera
 from image_processing.image_processing import ImageProcessor
 from data.excel_utils import ExcelUtils
+from PIL import Image, ImageTk
+import cv2
 
 class MainApplication:
     def __init__(self, root):
@@ -64,11 +65,26 @@ class MainApplication:
             self.excel_utils.load_excel_data(file_path)
 
     def capture_image(self):
-        # Действие по кнопке для захвата изображения
+        ret, frame = cap.read()
+        if ret:
+            global x1, y1, x2, y2
+            cropped_frame = frame[y1:y2, x1:x2]
+            cv2.imwrite('captured_frame.jpg', cropped_frame)
+            update_captured_image()
         pass
 
     def update_image(self):
-        # Обновление изображения с камеры в реальном времени
+        # Получаем текущий кадр из камеры
+        frame = self.camera.get_frame()
+        # Конвертируем BGR изображение, полученное с камеры, в формат RGB
+        cv2image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        # Переводим его в формат, совместимый с Tkinter
+        img = Image.fromarray(cv2image)
+        imgtk = ImageTk.PhotoImage(image=img)
+        self.video_label.imgtk = imgtk  # Удерживаем ссылку на изображение
+        self.video_label.configure(image=imgtk)
+        # Устанавливаем интервал в миллисекундах для следующего обновления
+        self.video_label.after(10, self.update_image)        
         pass
 
     def run(self):
