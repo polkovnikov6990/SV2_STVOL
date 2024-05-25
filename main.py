6752f3bdc682f32892833534ddc7037e23ae10a3
Alexey <polkovnikov6990@mail.ru>
2024-05-25 15:25:38 +0500

    Исправление ошибок в Application

--- main.py
+++ main.py
@@ -4,8 +4,21 @@ from gui.app_gui import MainApplication
 def main():
     root = tk.Tk()
     root.title("Камера")
+
+    # Создаем экземпляр приложения
     app = MainApplication(root)
     app.run()
+    
+    # Обработчик закрытия окна
+    def on_close():
+        # Здесь вызовите метод release для камеры
+        app.camera.release()
+        root.destroy()
+
+    # При закрытии окна будет вызываться функция on_close
+    root.protocol("WM_DELETE_WINDOW", on_close)
+
+    root.mainloop()
 
 if __name__ == "__main__":
     main()
